02ec442f354db5104302f14fa704505185ceb742
Wermenson Vasconcelos <wermenson.vasconcelo@gmail.com>
2023-10-22 11:49:47 -0300

    ✨ feat: Controller do endpoint "food"

---
+++ src/main/java/com/example/cardapio/controller/FoodControler.java
@@ -0,0 +1,9 @@
+package com.example.cardapio.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("food")
+public class FoodControler {
+}
