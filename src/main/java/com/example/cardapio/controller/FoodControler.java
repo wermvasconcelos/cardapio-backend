93c2fe356c1bedb45f8a8adc4e8d6256fa4f8d25
Wermenson Vasconcelos <wermenson.vasconcelo@gmail.com>
2023-10-22 13:31:22 -0300

    ✨ feat: Método post do endpoint "food"

--- src/main/java/com/example/cardapio/controller/FoodControler.java
+++ src/main/java/com/example/cardapio/controller/FoodControler.java
@@ -2,11 +2,10 @@ package com.example.cardapio.controller;
 
 import com.example.cardapio.food.Food;
 import com.example.cardapio.food.FoodRepository;
+import com.example.cardapio.food.FoodRequestDTO;
 import com.example.cardapio.food.FoodResponseDTO;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -15,11 +14,14 @@ import java.util.List;
 public class FoodControler {
     @Autowired
     private FoodRepository repository;
+    @PostMapping
+    public void saveFood(@RequestBody FoodRequestDTO data){
+        Food foodData = new Food(data);
+        repository.save(foodData);
+    }
     @GetMapping
     public List<FoodResponseDTO> getAll(){
 
-        List<FoodResponseDTO> foodList = repository.findAll().stream().map(FoodResponseDTO::new).toList();
-        return foodList;
-
+        return repository.findAll().stream().map(FoodResponseDTO::new).toList();
     }
 }
