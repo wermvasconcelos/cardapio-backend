b8dc8bbec9a12d4f2ceab2674a2faf40807cc4ca
Wermenson Vasconcelos <wermenson.vasconcelo@gmail.com>
2023-10-22 12:20:40 -0300

    :heavy_plus_sign: build: JPA e Postgres

--- src/main/java/com/example/cardapio/controller/FoodControler.java
+++ src/main/java/com/example/cardapio/controller/FoodControler.java
@@ -1,9 +1,14 @@
 package com.example.cardapio.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("food")
 public class FoodControler {
+    @GetMapping
+    public void getAll(){
+
+    }
 }
