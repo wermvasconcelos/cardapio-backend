3ac70333f2aa1e37454d43880dfbda852c27c6bd
Wermenson Vasconcelos <wermenson.vasconcelo@gmail.com>
2023-10-22 13:08:54 -0300

    ✨ feat: Método getAll() do endpoint "food"

--- src/main/java/com/example/cardapio/controller/FoodControler.java
+++ src/main/java/com/example/cardapio/controller/FoodControler.java
@@ -1,14 +1,25 @@
 package com.example.cardapio.controller;
 
+import com.example.cardapio.food.Food;
+import com.example.cardapio.food.FoodRepository;
+import com.example.cardapio.food.FoodResponseDTO;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("food")
 public class FoodControler {
+    @Autowired
+    private FoodRepository repository;
     @GetMapping
-    public void getAll(){
+    public List<FoodResponseDTO> getAll(){
+
+        List<FoodResponseDTO> foodList = repository.findAll().stream().map(FoodResponseDTO::new).toList();
+        return foodList;
 
     }
 }
